342b68a828846a65c780e9dfd0783bd6718901a9
frozenopertor <adityagupta11112002@gmail.com>
2024-02-07 00:44:38 +0530

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./routes/SignIn";
+import SignUp from "./routes/SignUp";
+import Profile from "./routes/Profile";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
 function App() {
   return (
-    <>
-    <div className=''></div>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
