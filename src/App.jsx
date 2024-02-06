025a769ccd0efcd2437238c4c211ce67bc87fb2e
frozenopertor <adityagupta11112002@gmail.com>
2024-02-07 01:20:06 +0530

    created routes and header section

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import SignUp from "./routes/SignUp";
 import Profile from "./routes/Profile";
 import About from "./routes/About";
 import Home from "./routes/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
+      {/* this is done so that header component appears in all the pages*/}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
